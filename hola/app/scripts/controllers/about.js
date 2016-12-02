406aad20ae3bad59519d0549ab51ed0f70f5bc2d
ivan alvarez <ivanAlvarez21@users.noreply.github.com>
2016-12-02 15:37:54 -0600

    Add files via upload

---
+++ hola/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name holaApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the holaApp
+ */
+angular.module('holaApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
